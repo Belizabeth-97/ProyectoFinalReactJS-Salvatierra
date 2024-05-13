c5267a86e13a00000599bba5657250e520c57bb3
Belizabeth-97 <brendaelizabethsalvatierra@gmail.com>
2024-05-13 14:55:13 -0300

    UseState, UseEffect

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,24 +1,18 @@
 import CardProducts from "./CardProducts"
+import React, { useState, useEffect } from 'react';
 
 /* Container debe llevar un efecto y un estado */
 
 function ItemListContainer() {
-  // Lista de productos
-  const productos = [
-    { nombre: 'Simple Pepinillo', id: 1 },
-    { nombre: 'Simple Cheddar', id: 2 },
-    { nombre: 'Simple Bacon', id: 3 },
-    { nombre: 'Doble Pepinillo', id: 4 },
-    { nombre: 'Doble Cheddar', id: 5 },
-    { nombre: 'Doble Bacon', id: 6 },
-    { nombre: 'Triple Pepinillo', id: 7 },
-    { nombre: 'Triple Cheddar', id: 8 },
-    { nombre: 'Triple Bacon', id: 9 },
-    { nombre: 'Papas fritas', id: 10 },
-    { nombre: 'Papas con Cheddar', id: 11 },
-    { nombre: 'Papas con Cheddar y Bacon', id: 12 },
-  ];
+  const [productos, setProductos] = useState([]);
 
+    useEffect(() => {
+        fetch('/src/components/api.json')
+            .then(response => response.json())
+            .then(data => setProductos(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+  
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {productos.map((producto) => (
