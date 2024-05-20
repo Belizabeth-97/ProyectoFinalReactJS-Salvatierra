6d8834ee0c23f5e290555ec7418bc09d4a0872d3
Belizabeth-97 <brendaelizabethsalvatierra@gmail.com>
2024-05-20 15:11:51 -0300

    Instalación Firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzYhChGK3Kz1FIerbBtO2TkHFFi5lfS5o",
+  authDomain: "reactjs-comision-54090.firebaseapp.com",
+  projectId: "reactjs-comision-54090",
+  storageBucket: "reactjs-comision-54090.appspot.com",
+  messagingSenderId: "730199685500",
+  appId: "1:730199685500:web:99137ae056574912db8cf9"
+};
+
+
+const app = initializeApp(firebaseConfig);
